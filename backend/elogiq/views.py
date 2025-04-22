344dfb45f7ef187c118ed42651aae639de5a7fb9
Mattia Antonini <mattia.antonini1995@gmail.com>
2025-04-22 12:29:21 +0200

    First version of backend

--- backend/elogiq/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.response import Response
-
-class TokenObtainAccessOnlyView(TokenObtainPairView):
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        # Get the access token from the response
-        access_token = response.data.get('access')
-        return Response({'token': access_token})

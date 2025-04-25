888e7dfc11d6e8e1c63dccf15bef0b32414fb155
Mattia Antonini <mattia.antonini1995@gmail.com>
2025-04-25 11:32:32 +0200

    New version of ui

--- frontend/dist/frontend/server/angular-app-manifest.mjs
+++ frontend/dist/frontend/server/angular-app-manifest.mjs
@@ -46,8 +46,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5548, hash: 'c511811debf01ded28e97c8db6853344200a0b2d33454d69b4d7d10acbc4ae57', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1261, hash: '01890c23e8d1896184da340aa629c341050a03227216276789b8f424f1fbd1c7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5548, hash: '1cf2c7440adbbc7f261e46ab2b612b577f7b2314a59550b0cf73e802d1813288', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1261, hash: '89a27a35ab4ece71b096f9fda0394f43d47ce7771a7d5f29df3caf64c68c3dab', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-YGYTPPG3.css': {size: 310884, hash: 'h4H4K3RyHno', text: () => import('./assets-chunks/styles-YGYTPPG3_css.mjs').then(m => m.default)}
   },
 };

715464931ff2b90376f7290ebd40991f1a273598
Mattia Antonini <mattia.antonini1995@gmail.com>
2025-04-25 09:37:40 +0200

    Fixing versions

--- backend/build.sh
+++ backend/build.sh
@@ -4,11 +4,6 @@ source .docker/.env
 
 echo "Build project as image with name: $IMAGE_NAME:$IMAGE_TAG"
 docker build -t $IMAGE_NAME:$IMAGE_TAG -f .docker/Dockerfile .
-docker tag $IMAGE_NAME:$IMAGE_TAG bistaff95/$IMAGE_NAME:$IMAGE_TAG
-docker push bistaff95/$IMAGE_NAME:$IMAGE_TAG
-
-docker tag postgres:16.8-bullseye bistaff95/elogiqpg:$IMAGE_TAG
-docker push bistaff95/elogiqpg:$IMAGE_TAG
 
 if [ $? -eq 0 ]; then
   echo "Image built: $IMAGE_NAME:$IMAGE_TAG"
--- frontend/build.sh
+++ frontend/build.sh
@@ -7,8 +7,6 @@ npm run build --configuration=production
 
 echo "Build project as image with name: $IMAGE_NAME:$IMAGE_TAG"
 docker build -t $IMAGE_NAME:$IMAGE_TAG -f .docker/Dockerfile .
-docker tag $IMAGE_NAME:$IMAGE_TAG bistaff95/$IMAGE_NAME:$IMAGE_TAG
-docker push bistaff95/$IMAGE_NAME:$IMAGE_TAG
 
 if [ $? -eq 0 ]; then
   echo "Image built: $IMAGE_NAME:$IMAGE_TAG"
